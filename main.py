59d59ed1fac7c6ebfe7a37b081b376ec8335ece6
xhomar <xhomar@proton.me>
2025-04-09 23:10:36 +0200

    particles velocity, minor fixes and improvements

--- main.py
+++ main.py
@@ -11,25 +11,41 @@ pygame.display.set_caption("Particle Life")
 
 SCREEN = pygame.display.set_mode((1280, 720))
 SCREEN_WIDTH, SCREEN_HEIGHT = SCREEN_SIZE = pygame.display.get_window_size()
-SIMULATION = pygame.Surface((128, 72))
+SIMULATION = pygame.Surface((1280, 720))
 SIMULATION_WIDTH, SIMULATION_HEIGHT = SIMULATION_SIZE = SIMULATION.get_size()
 CLOCK = pygame.Clock()
+deltatime = 0
 
 # particles forces setups
-force_decay = 50
+force_decay = 100
 
 # particles amount
 PARTICLES_GROUPS = 2
-PARTICLES_AMOUNT = 200
+PARTICLES_AMOUNT = 1000
 PARTICLES_GROUP = int(PARTICLES_AMOUNT / PARTICLES_GROUPS)
 
-# particles groups
-particles_position = np.array([np.array([randint(0, SIMULATION_WIDTH - 1) for _ in range(0, PARTICLES_GROUP * PARTICLES_GROUPS)]), np.array([randint(0, SIMULATION_HEIGHT - 1) for _ in range(0, PARTICLES_GROUP * PARTICLES_GROUPS)])])
-particles_velocity = np.array([np.zeros(PARTICLES_GROUP * PARTICLES_GROUPS), np.zeros(PARTICLES_GROUP * PARTICLES_GROUPS)])
-particles_render = np.array([pygame.Surface((1, 1)) for _ in range(0, PARTICLES_GROUPS)])
+def start():
+    # particles groups
+    global particles_position, particles_velocity, particles_render, particles_matrix
+    particles_position = np.array([
+        np.random.choice(np.arange(0, SIMULATION_WIDTH), size=PARTICLES_GROUP * PARTICLES_GROUPS),
+        np.random.choice(np.arange(0, SIMULATION_HEIGHT), size=PARTICLES_GROUP * PARTICLES_GROUPS)
+    ])
 
-# interaction matrix
-particles_attraction = np.random.randint(-100, 101, (PARTICLES_GROUPS, PARTICLES_GROUP))
+    particles_velocity = np.array([
+        np.zeros(PARTICLES_GROUP * PARTICLES_GROUPS),
+        np.zeros(PARTICLES_GROUP * PARTICLES_GROUPS)
+    ])
+    particles_render = np.array([pygame.Surface((1, 1)) for _ in range(0, PARTICLES_GROUPS)])
+
+    # interaction matrix
+    particles_matrix = np.array([
+        np.random.choice(np.arange(-100, 101), size=PARTICLES_GROUPS),
+        np.random.choice(np.arange(-100, 101), size=PARTICLES_GROUPS)
+    ])
+
+
+start()
 
 # setting color to particles groups
 color_range = int(355 / PARTICLES_GROUPS)
@@ -46,23 +62,29 @@ def accelaration():
     for particle in range(0, PARTICLES_GROUP * PARTICLES_GROUPS):
         delta_x = particles_position[0, :] - particles_position[0, particle]
         delta_y = particles_position[1, :] - particles_position[1, particle]
-        distances = ((delta_x ** 2) + (delta_y ** 2)) ** 0.5
+        distances = np.sqrt(((delta_x ** 2) + (delta_y ** 2)))
         directions = np.nan_to_num(np.array([delta_x / distances, delta_y / distances]), nan=0.0)
 
         # to do: set 1 to particles_attraction
         forces = (1 * (e ** -(distances / force_decay))) * directions
         acceleration = np.array([np.sum(forces[0]), np.sum(forces[1])])
 
-        acceleration_x = particles_position[0][particle] + acceleration[0]
-        acceleration_y = particles_position[1][particle] + acceleration[1]
-        if acceleration_x > SIMULATION_WIDTH - 1 or acceleration_x < 0:
-            acceleration_x = -acceleration_x
+        particles_velocity[0][particle] += acceleration[0] * deltatime
+        particles_velocity[1][particle] += acceleration[1] * deltatime
 
-        if acceleration_y > SIMULATION_WIDTH - 1 or acceleration_y < 0:
-            acceleration_y = -acceleration_y
+        velocity_x = particles_position[0][particle] + (particles_velocity[0][particle] * deltatime)
+        velocity_y = particles_position[1][particle] + (particles_velocity[1][particle] * deltatime)
 
-        particles_position[0][particle] = acceleration_x
-        particles_position[1][particle] = acceleration_y
+        if velocity_x > SIMULATION_WIDTH - 1 or velocity_x < 0:
+            particles_velocity[0][particle] = -particles_velocity[0][particle]
+            velocity_x = particles_position[0][particle] + (particles_velocity[0][particle] * deltatime)
+
+        if velocity_y > SIMULATION_HEIGHT - 1 or velocity_y < 0:
+            particles_velocity[1][particle] = -particles_velocity[1][particle]
+            velocity_y = particles_position[1][particle] + (particles_velocity[1][particle] * deltatime)
+
+        particles_position[0][particle] = velocity_x
+        particles_position[1][particle] = velocity_y
 
 
 while True:
@@ -74,10 +96,9 @@ while True:
             sys.exit()
         # key down
         if event.type == pygame.KEYDOWN:
-            # randomize particles
+            # randomize particles (fix this later)
             if event.key == pygame.K_r:
-                particles_position = np.array(
-                    [np.array([randint(0, SIMULATION_WIDTH - 1) for _ in range(0, PARTICLES_GROUP * PARTICLES_GROUPS)]),np.array([randint(0, SIMULATION_HEIGHT - 1) for _ in range(0, PARTICLES_GROUP * PARTICLES_GROUPS)])])
+                start()
     accelaration()
     # particles render in SIMULATION surface
     SIMULATION.fill((0, 0, 0))
@@ -90,4 +111,4 @@ while True:
     simulation_render = pygame.transform.scale(SIMULATION, SCREEN_SIZE)
     SCREEN.blit(simulation_render)
     pygame.display.update()
-    CLOCK.tick(24)
+    deltatime = CLOCK.tick(24) / 1000
